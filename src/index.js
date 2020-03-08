edde834a40a47f5453b78e763e26d4e56f7f131e
Humano Laranja <contato.carlos@outlook.com>
2020-03-08 14:41:20 -0300

    first version

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const utils = require("./utils");
+const app = express();
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
+app.get("/", async (req, res) => {
+  const data = await utils.getData();
+  res.json(data);
+});
+
+app.get("/world", async (req, res) => {
+  const data = await utils.getData();
+  const transfomedData = utils.parseData(data, "world");
+  res.json(transfomedData);
+});
+
+app.get("/world/total", async (req, res) => {
+  const data = await utils.getData();
+  const transfomedData = utils.parseData(data, "world", true);
+  res.json(transfomedData.total);
+});
+
+app.get("/world/last", async (req, res) => {
+  const data = await utils.getData();
+  const transfomedData = utils.parseData(data, "world", true);
+  res.json(transfomedData);
+});
+
+app.get("/brazil", async (req, res) => {
+  const data = await utils.getData();
+  const transfomedData = utils.parseData(data, "brazil");
+  res.json(transfomedData);
+});
+
+app.get("/brazil/total", async (req, res) => {
+  const total = await utils.getTotal();
+  res.json(total);
+});
+
+app.get("/brazil/last", async (req, res) => {
+  const data = await utils.getData();
+  const transfomedData = utils.parseData(data, "brazil", true);
+  res.json(transfomedData);
+});
---
+++ src/utils/index.js
@@ -0,0 +1,64 @@
+const fs = require("fs");
+const axios = require("axios");
+const codes = JSON.parse(fs.readFileSync("src/assets/codes.json", "utf8"));
+
+const getData = async () => {
+  const raw = await _getRaw();
+  const data = _getJson(raw);
+  return data;
+};
+
+const getTotal = async () => {
+  const data = await getData();
+  const transfomedData = parseData(data, "brazil", true);
+  const output = {
+    suspects: 0,
+    refuses: 0,
+    cases: 0,
+    deaths: 0
+  };
+
+  output.suspects = _sum(transfomedData.values, "suspects");
+  output.refuses = _sum(transfomedData.values, "refuses");
+  output.cases = _sum(transfomedData.values, "cases");
+  output.deaths = _sum(transfomedData.values, "deaths");
+
+  return output;
+};
+
+const parseData = (data, mode, last = false) => {
+  last ? (data = data[mode][data[mode].length - 1]) : (data = data[mode]);
+
+  if (last) {
+    data.values.forEach(element => {
+      const country = codes.find(item => item.code === element.uid);
+      country ? (element.name = country.name) : (element.name = "Unknown");
+    });
+  } else {
+    data.map(item =>
+      item.values.forEach(element => {
+        const country = codes.find(item => item.code === element.uid);
+        country ? (element.name = country.name) : (element.name = "Unknown");
+      })
+    );
+  }
+
+  return data;
+};
+
+const _getRaw = async () => {
+  var raw = await axios.get(
+    "http://plataforma.saude.gov.br/novocoronavirus/resources/scripts/database.js"
+  );
+  return raw.data;
+};
+
+const _getJson = raw => {
+  return JSON.parse(raw.split("=")[1]);
+};
+
+const _sum = (data, key) => {
+  return data.reduce((a, b) => a + (b[key] || 0), 0);
+};
+
+module.exports = { getData, getTotal, parseData };
