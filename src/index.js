5d3ab09a9c84355fb87120dbb79ec4f9f66ff11c
Humano Laranja <contato.carlos@outlook.com>
2020-03-17 15:16:10 -0300

    cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const utils = require("./utils");
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
